8dbe84f8544f2057fe366a13f9af07edb40691f3
Wellington <wellington.maeda@gmail.com>
2019-06-12 10:18:11 -0300

    terceiro commit

--- Aula_01.java
+++ Aula_01.java
@@ -8,7 +8,6 @@ public class Aula_01{
     public static void main(String[] args){
         
         System.out.println("Hello world");
-        System.out.println("ola mundo");
         
     }
     
